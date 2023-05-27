e4d86489da7c38d44a95b1204f5df8a93df3010d
JustWeiHao <e0960531@u.nus.edu>
2023-05-27 17:35:52 +0800

    Changed backend API into JavaScript

--- backend/src/login.js
+++ backend/src/login.js
@@ -1,17 +1,38 @@
-export function login(inputs, success, failed) {
+import { connection } from "./connector.js";
+import bcrypt from "bcryptjs";
+
+export async function login(inputs, response) {
+  const table_name = "Users";
 	try {
-	if (inputs == undefined) {
-      return failed("Inputs cannot be emptied!");
+	  if (inputs == undefined || Object.entries(inputs).length == 0) {
+      return(response(500, "Inputs cannot be emptied!"));
     }
-    if (!("username" in inputs)) {
-      return failed("Username field is not present!");
+    if (!("Username" in inputs)) {
+      return(response(500, "Username field is not present!"));
     }
-    if (!("password" in inputs)) {
-      return failed("Password field is not present!");
+    if (!("Password" in inputs)) {
+      return(response(500, "Password field is not present!"));
     }
-    return success("");
-	} catch (Error) {
-		console.log(Error);
-		return failed("Internal Server Error!");
+
+    const Username = inputs["Username"];
+    const Password = inputs["Password"];
+
+    let query = `SELECT Password FROM ${table_name} WHERE Username = ?`;
+    let res = "default";
+    const [result, fields] = await connection.query(query, [Username]);
+    if (result.length == 0) {
+          res = (response(401, "User does not exists!"));
+    } else {
+      const password_check = await bcrypt.compare(Password, result[0]["Password"]);
+      if (password_check) {
+        res = (response(200, "Login Successful!"));
+      } else {
+        res = (response(401, "Incorrect Password!"));
+      }
+    }
+    return res;
+	} catch (error) {
+		console.log(error);
+		return (response(500, "Error occured while accessing database!"));
 	}
 }
