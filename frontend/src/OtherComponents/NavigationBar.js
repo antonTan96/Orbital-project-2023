392fc5ab365cdea4202e81c9cc4ca9dea9f5156e
antonTan96 <antonhz2021@gmail.com>
2023-07-22 21:20:23 +0800

    Added navigation bar

---
+++ frontend/src/OtherComponents/NavigationBar.js
@@ -0,0 +1,84 @@
+import "./../CSSComponents/navbar.css";
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+function NavigationBar(props) {
+  const [open, changeState] = useState(false);
+  const navigate = useNavigate();
+  const toggleButton = useRef(null);
+  const sidebar = useRef(null);
+  const user = useParams();
+
+  useEffect(() => {
+    if (toggleButton.current) {
+      toggleButton.current.addEventListener("click", () => {
+        console.log("show the fucking navbar");
+
+        sidebar.current.classList.toggle("show");
+      });
+    }
+  }, [open]);
+
+  return (
+    <>
+      {!open && (
+        <div
+          ref={toggleButton}
+          onClick={() => changeState(!open)}
+          className="navbarButton"
+        >
+          <div className="icon"></div>
+          <div className="icon"></div>
+          <div className="icon"></div>
+        </div>
+      )}
+      <div className="navbar" ref={sidebar}>
+        <div
+          className="content"
+          onClick={() => {
+            console.log("ok");
+            navigate(`../${user.user}/Menu`);
+          }}
+        >
+          Current Task
+        </div>
+        <div
+          className="content"
+          onClick={() => {
+            console.log("To Task List");
+            navigate(`../${user.user}/AllTasks`);
+          }}
+        >
+          Task List
+        </div>
+        <div
+          className="content"
+          onClick={() => {
+            console.log("To Task List");
+            navigate(`../${user.user}/Contacts`);
+          }}
+        >
+          Contacts
+        </div>
+        <div
+          className="content"
+          onClick={() => {
+            console.log("return to login page");
+            navigate("../");
+          }}
+        >
+          Log Out
+        </div>
+      </div>
+      {open && (
+        <div
+          className="overlay navbarButton"
+          ref={toggleButton}
+          onClick={() => changeState(!open)}
+        ></div>
+      )}
+    </>
+  );
+}
+
+export default NavigationBar;
